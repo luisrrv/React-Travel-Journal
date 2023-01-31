2c4c9a5cae9a1d81959d220e09b73cbe1c677b6a
luis rodriguez <rodluisr@gmail.com>
2023-01-31 23:30:19 +0900

    details page popups, imgs & basic styles

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,65 +1,81 @@
 import React from 'react'
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import Footer from './Footer';
 
 
 // const images = require.context('../../public/images', true);
 
 const Details = (props) => {
     const item = useLocation().state;
-    console.log(item);
-    const [state, setState] = useState('pics');
+    const [map, setMap] = useState('off');
     const [details, setDetails] = useState('off');
-    const updateState = ()=> {
-        state==='pics' ? setState('map') :setState('pics');
-        console.log(state);
+    const updateMap = ()=> {
+        map==='off' ? setMap('on') && setDetails('off') : setMap('off');
+        details==='on' && setDetails('off');
+    }
+    const closeMap = ()=> {
+        setMap('off');
     }
     const updateDetails = ()=> {
-        details==='off' ? setDetails('on') :setDetails('off');
-        console.log(details);
+        details==='off' ? setDetails('on') : setDetails('off');
+        map==='on' && setMap('off');
+    }
+    const detailsOff = ()=> {
+        setDetails('off');
     }
     const locationImg = {
         backgroundImage: `url(${item.coverImg})`,
-        height: '400px',
-        width: '400px',
+        height: '200px',
+        width: '200px',
         backgroundSize: 'cover',
+        backgroundPositionX: 'center',
+        backgroundPositionY: 'center',
       };
     return (
         <div className="details">
-            <Link to='/' >{"<Travels list"}</Link>
+            <div className="nav">
+              <Link to='/' className="back-btn">{"back to the list"}</Link>
+              <p className="about" onClick={updateDetails}>{`About ${item.location}`}</p>
+            </div>
             <div className="header">
                 <h1>{item.title}</h1>
-                <p className="about" onClick={updateDetails}>{`About ${item.location}`}</p>
                 <p className="dates">{`${item.dates.start} - ${item.dates.end}`}</p>
                 <div className="location-img" style={locationImg}></div>
             </div>
-            { state === 'map' ? <div className="btn" onClick={updateState}>View pics</div> : <div className="btn" onClick={updateState}>View map</div> }
+            <div className="btn" onClick={updateMap}>Map</div>
             <div className="content">
-                { 
-                (
-                    state === 'map' ? 
-                    <div className="map">
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                        Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
-                    </div> 
-                    : 
                     <div className="pics">
-                        Didn't take any pics :(
-                    </div> 
-                    )
-                }
+                        <img src="https://www.boredpanda.com/blog/wp-content/uploads/2016/01/manny-cat-takes-selfies-dogs-gopro-9.jpg" alt="selfie" />
+                        <img src="https://i.pinimg.com/originals/f3/9b/49/f39b499a328dbba5a5f63671250b4f5c.png" alt="majestic" />
+                        <img src="https://preview.redd.it/u6ckdfs0jto11.jpg?width=640&crop=smart&auto=webp&s=585c61f7d764906f2af3f4c91ab5ab53b69ad88f" alt="lizzard" />
+                        <img src="https://www.boredpanda.com/blog/wp-content/uploads/2016/01/manny-cat-takes-selfies-dogs-gopro-9.jpg" alt="selfie" />
+                        <img src="https://i.pinimg.com/originals/f3/9b/49/f39b499a328dbba5a5f63671250b4f5c.png" alt="majestic" />
+                        <img src="https://preview.redd.it/u6ckdfs0jto11.jpg?width=640&crop=smart&auto=webp&s=585c61f7d764906f2af3f4c91ab5ab53b69ad88f" alt="lizzard" />
+                        <img src="https://www.boredpanda.com/blog/wp-content/uploads/2016/01/manny-cat-takes-selfies-dogs-gopro-9.jpg" alt="selfie" />
+                        <img src="https://i.pinimg.com/originals/f3/9b/49/f39b499a328dbba5a5f63671250b4f5c.png" alt="majestic" />
+                        <img src="https://preview.redd.it/u6ckdfs0jto11.jpg?width=640&crop=smart&auto=webp&s=585c61f7d764906f2af3f4c91ab5ab53b69ad88f" alt="lizzard" />
+                    </div>
             </div>
-            
-            { details==='on' && <div className="popup details">{item.description}</div> }
-            
+
+            {(map === 'on' &&
+                <div className="popup map-container">
+                  <div onClick={closeMap} className="x">X</div>
+                  <div className="map">
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                    Map Map Map Map Map Map Map Map Map Map Map Map Map <br />
+                  </div>
+                </div>)}
+            { details==='on' && <div className="popup details">{item.description}<div onClick={detailsOff} className='x'>X</div></div> }
+            <Footer />
         </div>
     )
 }
