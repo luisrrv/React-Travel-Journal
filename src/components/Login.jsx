fd55d9d2208fcb8ab0ece3af4e5aaca4445a20b3
luisrrv <rodluisr@gmail.com>
2023-02-08 11:13:20 +0900

    favicon&fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 // import { styled } from '@mui/material/styles';
 
 function Login({ setEmail, setPassword, loginFormOn, handleAction, blurSet}) {
@@ -14,41 +15,48 @@ function Login({ setEmail, setPassword, loginFormOn, handleAction, blurSet}) {
     }, 1000);
   }
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
   return (
     <div className='login-form' onClick={loginFormOn} >
             <div className="heading-container">
                 <h3>Are you luisrrv ?</h3>
             </div>
+            <ThemeProvider theme={darkTheme}>
 
-            <Box
-                component="form"
-                sx={{
+              <Box
+                  component="form"
+                  sx={{
                     '& > :not(style)': { m: 1 },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-              <div className='manager-login-form'>
-              <TextField
-              id="email"
-              label="Email"
-              variant="outlined"
-              type="email"
-              className={'login-text-form'}
-              onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                id="password"
-                label="Password"
-                type="password"
+                  }}
+                  noValidate
+                  autoComplete="off"
+                  >
+                <div className='manager-login-form'>
+                <TextField
+                id="email"
+                label="Email"
                 variant="outlined"
-                className='login-text-form'
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              </div>
+                type="email"
+                className={'login-text-form'}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                  id="password"
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  className='login-text-form'
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
 
-            </Box>
+              </Box>
+            </ThemeProvider>
 
             <Button
               variant="contained"
