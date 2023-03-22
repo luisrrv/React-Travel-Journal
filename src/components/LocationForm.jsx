d907751bb165054e98695e4c0ab66c6193030240
luisrrv <rodluisr@gmail.com>
2023-03-22 16:49:03 +0900

    logged in users can add new locations from top menu

--- src/components/LocationForm.jsx
+++ src/components/LocationForm.jsx
@@ -8,7 +8,7 @@ import { collection, getDocs, addDoc } from 'firebase/firestore'
 // import { styled } from '@mui/material/styles';
 
 function LocationForm({locationFormOn, blurSet, db, newId}) {
-  const [disabled, setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(true);
 
   const usersCollectionRef = collection(db, "Places");
   const [allLocations, setAllLocations] = useState([]);
@@ -19,54 +19,55 @@ function LocationForm({locationFormOn, blurSet, db, newId}) {
   const [dateStart, setDateStart] = useState('');
   const [dateEnd, setDateEnd] = useState('');
   const [location, setLocation] = useState('');
-  const [pics, setPics] = useState([]);
-  const [pipts, setPipts] = useState(
-    [
-        <TextField
-            required
-            id="pics"
-            label="Picture url"
-            type="pics"
-            variant="outlined"
-            className='login-text-form'
-            onChange={(e) => setPics(pics.push(e.target.value))}
-        />
-  ]);
-  console.log(pipts)
-
-  const createLocation = async () => {
-    setDates({start: dateStart, end: dateEnd});
-    await addDoc(usersCollectionRef, {
-        my_id: newId,
-        title: title, 
-        description: description,
-        cover_img: coverImg,
-        dates: dates,
-        location: location,
-        pics: pics,
-    })
-    const data = await getDocs(usersCollectionRef);
-    console.log(data);
-    setAllLocations(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
-  };
-
-  const addPicInput = () => {
-    if(pipts.length>9) return;
-    setPipts(pipts.push(
-        <TextField
-            required
-            key={pipts.length}
-            id="pics"
-            label="Picture url"
-            type="pics"
-            variant="outlined"
-            className='login-text-form'
-            onChange={(e) => setPics(pics.push(e.target.value))}
-        />
-    ));
-    console.log(pipts);
+  const [pics, setPics] = useState(['','','','','','','','','','']);
+  const [piptN, setPiptN] = useState(1);
+  
+    console.log(allLocations);
+  const iptsCheck = () => {
+    const ipts = document.querySelectorAll('input');
+    let filled = 0;
+    ipts.forEach(ipt=>{
+        ipt.value!=='' && (filled += 1);
+    });
+    filled === ipts.length ? setDisabled(false) : setDisabled(true);
+  }
+  const addpipt = ()=> {
+    piptN<10 && setPiptN(piptN + 1);
   }
 
+  const pushPics = (e,i)=> {
+    (i===0) && setPics([e,pics[1],pics[2],pics[3],pics[4],pics[5],pics[6],pics[7],pics[8],pics[9]]);
+    (i===1) && setPics([pics[0],e,pics[2],pics[3],pics[4],pics[5],pics[6],pics[7],pics[8],pics[9]]);
+    (i===2) && setPics([pics[0],pics[1],e,pics[3],pics[4],pics[5],pics[6],pics[7],pics[8],pics[9]]);
+    (i===3) && setPics([pics[0],pics[1],pics[2],e,pics[4],pics[5],pics[6],pics[7],pics[8],pics[9]]);
+    (i===4) && setPics([pics[0],pics[1],pics[2],pics[3],e,pics[5],pics[6],pics[7],pics[8],pics[9]]);
+    (i===5) && setPics([pics[0],pics[1],pics[2],pics[3],pics[4],e,pics[6],pics[7],pics[8],pics[9]]);
+    (i===6) && setPics([pics[0],pics[1],pics[2],pics[3],pics[4],pics[5],e,pics[7],pics[8],pics[9]]);
+    (i===7) && setPics([pics[0],pics[1],pics[2],pics[3],pics[4],pics[5],pics[6],e,pics[8],pics[9]]);
+    (i===8) && setPics([pics[0],pics[1],pics[2],pics[3],pics[4],pics[5],pics[6],pics[7],e,pics[9]]);
+    (i===9) && setPics([pics[0],pics[1],pics[2],pics[3],pics[4],pics[5],pics[6],pics[7],pics[8],e,]);
+  }
+  
+  const createLocation = async () => {
+      setDates({start: dateStart, end: dateEnd});
+      const picsTaken = []; 
+      pics.forEach(e=>{
+        e!=='' && picsTaken.push(e);
+      });
+      await addDoc(usersCollectionRef, {
+          my_id: newId,
+          title: title, 
+          description: description,
+          cover_img: coverImg,
+          dates: dates,
+          location: location,
+          pics: picsTaken,
+        })
+        const data = await getDocs(usersCollectionRef);
+        console.log(data);
+        setAllLocations(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+    };
+        
   const handleClick = () => {
     setDisabled(true);
     setTimeout(() => {
@@ -99,81 +100,87 @@ function LocationForm({locationFormOn, blurSet, db, newId}) {
                 <div className='manager-login-form'>
                     <TextField
                     required
+                    key="title"
                     id="title"
                     label="Title"
                     placeholder='City, Country'
                     variant="outlined"
                     type="title"
                     className={'login-text-form'}
-                    onChange={(e) => setTitle(e.target.value)}
+                    onChange={(e) => {setTitle(e.target.value); iptsCheck();}}
                     />
                     <TextField
                     required
+                    key="description"
                     id="description"
                     label="Description"
                     placeholder=''
                     type="description"
                     variant="outlined"
                     className='login-text-form'
-                    onChange={(e) => setDescription(e.target.value)}
+                    onChange={(e) => {setDescription(e.target.value); iptsCheck();}}
                     />
                     <TextField
                     required
+                    key="coverImg"
                     id="coverImg"
                     label="Cover Image (url)"
                     type="coverImg"
                     variant="outlined"
                     className='login-text-form'
-                    onChange={(e) => setCoverImg(e.target.value)}
+                    onChange={(e) => {setCoverImg(e.target.value); iptsCheck();}}
                     />
                     <TextField
                     required
+                    key="dates1"
                     id="dates"
                     label="Trip start date"
                     placeholder='1 January, 2020'
                     type="dates"
                     variant="outlined"
                     className='login-text-form'
-                    onChange={(e) => setDateStart(e.target.value)}
+                    onChange={(e) => {setDateStart(e.target.value); iptsCheck();}}
                     />
                     <TextField
                     required
+                    key="dates2"
                     id="dates"
                     label="Trip end date"
                     placeholder='1 January, 2020'
                     type="dates"
                     variant="outlined"
                     className='login-text-form'
-                    onChange={(e) => setDateEnd(e.target.value)}
+                    onChange={(e) => {setDateEnd(e.target.value); iptsCheck();}}
                     />
                     <TextField
                     required
+                    key="location"
                     id="location"
                     label="Location (city)"
                     placeholder='CITY NAME'
                     type="location"
                     variant="outlined"
                     className='login-text-form'
-                    onChange={(e) => setLocation(e.target.value)}
+                    onChange={(e) => {setLocation(e.target.value); iptsCheck();}}
                     />
                     <h3>Pics taken during the trip</h3>
                     <div className='pics-inputs'>
                         {
-                            pipts.map((pipt)=>{
-                                return pipt;
+                            [...Array(piptN)].map((_e, i) => {
+                                return  <TextField
+                                            required
+                                            key={(i+1).toString()}
+                                            id={`pic${i+1}`}
+                                            label={`Picture ${i+1} url`}
+                                            type="pics"
+                                            variant="outlined"
+                                            className='login-text-form'
+                                            onChange={(e) => {pushPics(e.target.value,i); iptsCheck();}}
+                                        />
                             })
                         }
-                        {/* <TextField
-                        required
-                        id="pics"
-                        label="Picture url"
-                        type="pics"
-                        variant="outlined"
-                        className='login-text-form'
-                        onChange={(e) => setPics(pics.push(e.target.value))}
-                        /> */}
                     </div>
-                    <Button onClick={addPicInput}>add more pics</Button>
+                    { piptN < 10 && <Button onClick={addpipt}>add more pics</Button> }
                 </div>
 
               </Box>
@@ -183,7 +190,7 @@ function LocationForm({locationFormOn, blurSet, db, newId}) {
               variant="contained"
               disabled={disabled}
               onClick={() => { createLocation(); handleClick(); blurSet(); }}
-              className="login-btn"
+              className="login-btn add-location-btn"
               >
               Add
             </Button>
